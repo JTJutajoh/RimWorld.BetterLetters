4aea29e3901adf81bb680d53c76868048e26dc08
JT <jtjutajoh@gmail.com>
2025-06-14 18:53:31 -0700

    Fixed snoozed letter culling patch

--- source/BetterLettersMod.cs
+++ source/BetterLettersMod.cs
@@ -21,7 +21,7 @@ namespace BetterLetters
         {
             Instance = this;
             LogPrefixed.Initialize(this, "cyan");
-            
+
             GetSettings<Settings>();
         }
 
@@ -30,14 +30,14 @@ namespace BetterLetters
             try
             {
                 base.DoSettingsWindowContents(inRect);
-                GetSettings<Settings>().DoWindowContents(inRect);;
+                GetSettings<Settings>().DoWindowContents(inRect);
             }
             catch (Exception e)
             {
                 LogPrefixed.Exception(e, "Error drawing mod settings window.", true);
             }
         }
-        
+
         public override string SettingsCategory()
         {
             return "BetterLetters_SettingsCategory".Translate();
@@ -48,6 +48,7 @@ namespace BetterLetters
     internal static class LoadHarmony
     {
         private static Harmony _harmony;
+
         static LoadHarmony()
         {
             _harmony = new Harmony(BetterLettersMod.Instance!.Content.PackageId);
@@ -64,7 +65,8 @@ namespace BetterLetters
             }
             catch (Exception e)
             {
-                LogPrefixed.Exception(e, "Error patching vanilla. This likely means either the wrong game version or a hard incompatibility with another mod.");
+                LogPrefixed.Exception(e,
+                    "Error patching vanilla. This likely means either the wrong game version or a hard incompatibility with another mod.");
             }
             // Do any mod-specific patching (Vanilla Expanded...)
 
@@ -80,7 +82,7 @@ namespace BetterLetters
             var type = typeof(Verse.ChoiceLetter);
             // The class that the patches belong to
             var patchClass = typeof(RemoveLetterPatches);
-            
+
             PostfixGetter(type, patchClass, "Option_Close");
             PostfixGetter(type, patchClass, "Option_JumpToLocation");
             PrefixMethod(type, patchClass, "Option_ViewInQuestsTab");
@@ -101,9 +103,10 @@ namespace BetterLetters
             // Patch to clear any old letter reference when a dialog is opened
             patchClass = typeof(Dialog_NodeTreeConstructor);
             type = typeof(Dialog_NodeTree);
-            _harmony.Patch(type.GetConstructor(new Type[] { typeof(DiaNode), typeof(bool), typeof(bool), typeof(string) }),
+            _harmony.Patch(
+                type.GetConstructor(new Type[] { typeof(DiaNode), typeof(bool), typeof(bool), typeof(string) }),
                 postfix: GetPatch(patchClass, "ConstructorPostfix")
-                );
+            );
 
             // Patch Archive to add newly pinned letters back to the LetterStack
             patchClass = typeof(ArchivePinPatch);
@@ -123,14 +126,13 @@ namespace BetterLetters
                 type.GetMethod("DrawButtonAt", AccessTools.all),
                 postfix: GetPatch(patchClass, "DrawButtonAt_Postfix"),
                 transpiler: GetPatch(patchClass, "DrawButtonAt_Transpiler")
-                );
-            
-            //TODO: Figure out how to patch this since it's an INTERFACE method
-            // // Patch letters to not allow culling if they are snoozed
-            // patchClass = typeof(LetterCanCullArchivedNowPatch);
-            // type = typeof(Letter);
-            // PostfixGetter(type, patchClass, "CanCullArchivedNow");
-            
+            );
+
+            // Patch letters to not allow culling if they are snoozed
+            patchClass = typeof(LetterCanCullArchivedNowPatch);
+            type = typeof(Letter);
+            PostfixGetter(type, patchClass, "CanCullArchivedNow", interfaceType: typeof(IArchivable));
+
             // Patch the History window to show snoozed icon for snoozed letters
             patchClass = typeof(HistoryDoArchivableRowPatch);
             type = typeof(MainTabWindow_History);
@@ -146,16 +148,15 @@ namespace BetterLetters
             type = typeof(LetterStack);
 
 #if v1_1 || v1_2 || v1_3 || v1_4
-
             _harmony.Patch(
                 type.GetMethod("ReceiveLetter", new [] {typeof(Letter), typeof(string)}),
                 postfix: GetPatch(patchClass, "ReceiveLetter")
                 );
 #elif v1_5 || v1_6
             _harmony.Patch(
-                type.GetMethod("ReceiveLetter", new [] {typeof(Letter), typeof(string), typeof(int), typeof(bool)}),
+                type.GetMethod("ReceiveLetter", new[] { typeof(Letter), typeof(string), typeof(int), typeof(bool) }),
                 postfix: GetPatch(patchClass, "ReceiveLetter")
-                );
+            );
 #endif
         }
 
@@ -173,12 +174,34 @@ namespace BetterLetters
             }
         }
 
+        private static MethodInfo? GetGetter(Type implementingType, Type interfaceType, string propName)
+        {
+            try
+            {
+                LogPrefixed.Trace($"Patching interface property {propName} getter");
+                var interfaceProp = interfaceType.GetProperty(propName, AccessTools.all);
+                if (interfaceProp == null)
+                    return null;
+
+                var map = implementingType.GetInterfaceMap(interfaceType);
+                var interfaceMethod = interfaceProp.GetGetMethod(true);
+
+                var index = Array.IndexOf(map.InterfaceMethods, interfaceMethod);
+                return index != -1 ? map.TargetMethods[index] : null;
+            }
+            catch (Exception e)
+            {
+                LogPrefixed.Error($"Error getting interface getter for property: {implementingType.Name}.{propName}");
+                throw;
+            }
+        }
+
         private static HarmonyMethod GetPatch(Type t, string methodName)
         {
             try
             {
                 LogPrefixed.Trace($"Patching {methodName} method");
-                return new HarmonyMethod(t.GetMethod(methodName,AccessTools.all));
+                return new HarmonyMethod(t.GetMethod(methodName, AccessTools.all));
             }
             catch (Exception e)
             {
@@ -193,11 +216,11 @@ namespace BetterLetters
         /// <param name="t">The type to patch the property of</param>
         /// <param name="patchClass">A class containing patches with matching names to the desired properties</param>
         /// <param name="propName">The property to patch. Must match the method name in the patchClass.</param>
-        private static void PostfixGetter(Type t, Type patchClass, string propName)
+        private static void PostfixGetter(Type t, Type patchClass, string propName, Type? interfaceType = null)
         {
             try
             {
-                var original = GetGetter(t, propName);
+                var original = interfaceType is null ? GetGetter(t, propName) : GetGetter(t, interfaceType, propName);
                 var patch = GetPatch(patchClass, propName);
                 _harmony.Patch(original, postfix: patch);
             }
