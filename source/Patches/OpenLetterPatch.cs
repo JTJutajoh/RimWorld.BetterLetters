7b715293b512c945dfc29fedf11185f6394081a4
JT <jtjutajoh@gmail.com>
2025-06-16 17:43:29 -0700

    Formatting

--- source/Patches/OpenLetterPatch.cs
+++ source/Patches/OpenLetterPatch.cs
@@ -10,8 +10,12 @@ namespace BetterLetters.Patches
 {
     internal class OpenLetterPatch
     {
-        private static readonly MethodInfo? AnchorMethodAddRange = typeof(List<DiaOption>).GetMethod(nameof(List<DiaOption>.AddRange));
-        private static readonly MethodInfo? AnchorMethodAddToStack = typeof(WindowStack).GetMethod(nameof(WindowStack.Add));
+        private static readonly MethodInfo? AnchorMethodAddRange =
+            typeof(List<DiaOption>).GetMethod(nameof(List<DiaOption>.AddRange));
+
+        private static readonly MethodInfo? AnchorMethodAddToStack =
+            typeof(WindowStack).GetMethod(nameof(WindowStack.Add));
+
         /// <summary>
         /// General-purpose transpiler that can be applied to all vanilla implementations of Letter.OpenLetter
         /// Intercepts the list of choices sent to the dialog and adds a "Pin" option to the end of the list
@@ -31,19 +35,23 @@ namespace BetterLetters.Patches
 
                     // A reference to the Letter.Choices Property is on the stack, about to be passed to AddRange.
                     // This transpiler intercepts it and adds an option to it before it gets sent to AddRange.
-                    yield return new CodeInstruction(OpCodes.Ldarg_0);                      // Load a "this" reference onto the stack
+                    yield return new CodeInstruction(OpCodes.Ldarg_0); // Load a "this" reference onto the stack
                     yield return CodeInstruction.Call(typeof(OpenLetterPatch), nameof(AddChoices));
 
                     // About to execute:
                     // IL_0017: callvirt instance void class [mscorlib]System.Collections.Generic.List`1<class Verse.DiaOption>::AddRange(class [mscorlib]System.Collections.Generic.IEnumerable`1<!0>)
                 }
+
                 if (codes[i].Calls(AnchorMethodAddToStack))
                 {
                     // Save a reference to the current letter
                     // Do this last so that the constructor for the dialog (which just ran) can clear the reference
-                    yield return new CodeInstruction(OpCodes.Ldarg_0);                      // Load a "this" reference onto the stack
-                    yield return CodeInstruction.Call(typeof(OpenLetterPatch), nameof(SaveLetterReference)); // Send the reference to the current letter to the dialog patch
+                    yield return new CodeInstruction(OpCodes.Ldarg_0); // Load a "this" reference onto the stack
+                    yield return
+                        CodeInstruction.Call(typeof(OpenLetterPatch),
+                            nameof(SaveLetterReference)); // Send the reference to the current letter to the dialog patch
                 }
+
                 yield return codes[i];
             }
         }
@@ -63,11 +71,15 @@ namespace BetterLetters.Patches
         {
             var defaultText = "BetterLetters_DialogFloatMenuButton".Translate();
             var pinnedText = "BetterLetters_Unpin".Translate();
-            
-            var option = new DiaOption(__instance.IsPinned() ? pinnedText : defaultText)
-                {
-                    clickSound = __instance.IsPinned() ? SoundDefOf.Checkbox_TurnedOff : SoundDefOf.Checkbox_TurnedOn
-                };
+
+            var optionText = __instance.IsPinned() ? pinnedText :
+                __instance.IsSnoozed() ? "BetterLetters_CancelSnooze".Translate(SnoozeManager.Snoozes[__instance].Duration.ToStringTicksToPeriodVague(vagueMin: false)) :
+                defaultText;
+
+            var option = new DiaOption(optionText)
+            {
+                clickSound = __instance.IsPinned() ? SoundDefOf.Checkbox_TurnedOff : SoundDefOf.Checkbox_TurnedOn
+            };
             option.action = delegate
             {
                 if (__instance.IsPinned())
